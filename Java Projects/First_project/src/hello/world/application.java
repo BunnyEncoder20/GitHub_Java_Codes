a9d18903f058fc894f9cfb11e95ab57eeb21df77
BunnyEncoder20 <103634057+BunnyEncoder20@users.noreply.github.com>
2022-05-22 08:03:36 +0530

    practicing in the hello world class

---
+++ Java Projects/First_project/src/hello/world/application.java
@@ -0,0 +1,30 @@
+package hello.world;
+
+public class application {
+    public static void main(String[] args) {
+        // System.out.println("\nHello world using packages folder and a java class\n");
+
+        // int[] arrayname = {1,2,3};
+        // System.out.println(arrayname[0]);
+        // System.out.println(arrayname[1]);
+        // System.out.println(arrayname[2]);
+
+        // int[][] array2D = {{1,2},{3,4}};
+
+        // for (int i = 0; i < 2; i++) {
+        //     for (int j = 0; j < 2; j++) {
+        //         System.out.println(array2D[i][j]);
+        //     }
+        // }
+
+        String str = "Bunny and Senpai are";
+        System.out.println(str);
+
+        String str2 = new String(str);
+        System.out.println(str2+" best friends");
+
+        String str3 = new String();
+        System.out.println(str.length());
+
+    }
+}
--- Java Projects/First_project/src/hello_world.java
+++
@@ -1,5 +0,0 @@
-public class hello_world {
-    public static void main(String[] args) {
-        System.out.println("This is my first Java Project : Saying Hello World so...\nHello World !");
-    }    
-}
