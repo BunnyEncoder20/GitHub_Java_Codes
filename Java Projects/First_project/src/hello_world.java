01395e794a7ffd8d319208121592ef068c502335
BunnyEncoder20 <103634057+BunnyEncoder20@users.noreply.github.com>
2022-05-22 06:33:24 +0530

    making first java project

---
+++ Java Projects/First_project/src/hello_world.java
@@ -0,0 +1,5 @@
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("This is my first Java Project : Saying Hello World so...\nHello World !");
+    }    
+}
