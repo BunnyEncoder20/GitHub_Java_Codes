ec2296eff870c2b3ed9aac8b12f6668200677d0c
BunnyEncoder20 <103634057+BunnyEncoder20@users.noreply.github.com>
2022-06-09 13:59:24 +0530

    polished up about hashmaps and maps

--- HackerRank Questions/maps_dictionary.java
+++ HackerRank Questions/maps_dictionary.java
@@ -1,5 +1,11 @@
 import java.util.*;
 
+/*
+ *  Map is a interface in java 
+ *  Interfaces cannot have objects of their own
+ *  Hence a seperate class is made to implement Map which is HashMap
+ *  Hence when we use use Map, we create a object of class HashMap 
+ */
 public class maps_dictionary {
     public static void main(String[] args) {
         Map<String,Long> phonebookMap = new HashMap<String,Long>();
@@ -8,16 +14,25 @@ public class maps_dictionary {
         String temp1 = op.nextLine();
 
         for (int i = 0; i < keys; i++) {
-            String key = op.nextLine();
-            Long value = op.nextLong();
-            String temp2 = op.nextLine();
+            String input[] = op.nextLine().split(" "); 
+            String  key = input[0];
+            Long value = Long.parseLong(input[1]);
             phonebookMap.put(key, value);
         }
 
-        for (int i = 0; i < keys; i++) {
-            String find = op.nextLine();
-            if (phonebookMap.containsKey(find)) {
-                System.out.println(find+"="+phonebookMap.get(find));
+        //To display all the key - value entries :-
+        // for (String key : phonebookMap.keySet()) {
+        //     System.out.println("Key : " + key +"    Value : " + phonebookMap.get(key));
+        // }
+
+
+        //while(op.hasNext() keeps taking input till user enters one)
+        // ctrl+z+enter to exit from terminal
+        while (op.hasNext()) {
+            String key = op.nextLine();
+            
+            if (phonebookMap.containsKey(key)) {
+                System.out.println(key+"="+phonebookMap.get(key));
             } else {
                 System.out.println("Not found");
             }
