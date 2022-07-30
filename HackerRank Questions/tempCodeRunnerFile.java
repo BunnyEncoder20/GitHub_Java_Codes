a0dc93aa14c51af74b5c07884432dc834fcd9802
BunnyEncoder20 <103634057+BunnyEncoder20@users.noreply.github.com>
2022-07-30 13:12:16 +0530

    Delete tempCodeRunnerFile.java

--- HackerRank Questions/tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
-String s1; int x;
-        // for(int i=0;i<3;i++){
-        //     s1=sc.next();
-        //     x=sc.nextInt();
-        //     System.out.printf("%-15s %15s %n",s1,String.format("%03d",x)); ;
-        // }
