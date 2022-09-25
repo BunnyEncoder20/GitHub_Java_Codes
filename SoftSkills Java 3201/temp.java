7fd73317f9dc8b20c7305b7915078165232f8dc7
BunnyEncoder20 <103634057+BunnyEncoder20@users.noreply.github.com>
2022-09-25 11:18:15 +0530

    Temp file changed

--- SoftSkills Java 3201/temp.java
+++ SoftSkills Java 3201/temp.java
@@ -1,29 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class temp{
-    int x = 10;
-    int y = 40;
-    String msg = "Hello there, this is a class variable !";
-
-    public int sum(int a, int b) {
-        return a+b;
-    }
-
-    public static void main(String args[]) throws IOException{
-        temp obj = new temp();
-        BufferedReader op = new BufferedReader(new InputStreamReader(System.in));
-       int rows = Integer.parseInt(op.readLine());
-
-       for (int i = 0; i < rows; i++) {
-            for (int j = 2*(rows-i) ; j >=0 ; j--) {
+    public static void main(String[] args) {
+        // Scanner op = new Scanner(System.in);
+        int rows  = 10;
+        for (int i = 0; i < rows-1; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print(" ");
             }
-            for(int j=0;j<=i;j++)
+            for (int j = 0; j < rows-1-i; j++) {
                 System.out.print("* ");
+            }
             System.out.println();
-       }
-        op.close();
+        }
     }
 }
